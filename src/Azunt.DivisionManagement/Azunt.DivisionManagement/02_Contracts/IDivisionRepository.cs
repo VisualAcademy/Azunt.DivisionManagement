651ed0d890002ae84c8e94462cfee9a713a23aef
Park YongJune <redplus@redplus.net>
2025-05-31 17:50:41 +0900

    Add IDivisionRepository interface with async CRUD and paging methods

---
+++ src/Azunt.DivisionManagement/Azunt.DivisionManagement/02_Contracts/IDivisionRepository.cs
@@ -0,0 +1,13 @@
+﻿using Azunt.Models.Common;
+
+namespace Azunt.DivisionManagement;
+
+public interface IDivisionRepository
+{
+    Task<Division> AddAsync(Division model, string? connectionString = null);
+    Task<List<Division>> GetAllAsync(string? connectionString = null);
+    Task<Division> GetByIdAsync(long id, string? connectionString = null);
+    Task<bool> UpdateAsync(Division model, string? connectionString = null);
+    Task<bool> DeleteAsync(long id, string? connectionString = null);
+    Task<ArticleSet<Division, int>> GetArticlesAsync<TParentIdentifier>(int pageIndex, int pageSize, string searchField, string searchQuery, string sortOrder, TParentIdentifier parentIdentifier, string? connectionString = null);
+}
